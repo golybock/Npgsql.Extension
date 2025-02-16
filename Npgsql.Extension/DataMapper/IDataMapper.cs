7f8093d788a50ece8810c0cd85ea4c1191000943
golybock <teplovartem3094@gmail.com>
2025-02-16 15:49:35 +0300

    Global refactoring

---
+++ Npgsql.Extension/DataMapper/IDataMapper.cs
@@ -0,0 +1,14 @@
+﻿namespace Npgsql.Extension.DataMapper;
+
+public interface IDataMapper
+{
+	T? Map<T>(NpgsqlDataReader reader) where T : new();
+
+	IEnumerable<T> MapList<T>(NpgsqlDataReader reader) where T : new();
+
+	Task<T?> MapAsync<T>(NpgsqlDataReader reader) where T : new();
+
+	Task<IEnumerable<T>> MapListAsync<T>(NpgsqlDataReader reader) where T : new();
+
+	protected T MapObject<T>(NpgsqlDataReader reader) where T : new();
+}
--- Npgsql.Extension/Reader/IReader.cs
+++ Npgsql.Extension/Reader/IReader.cs
@@ -1,14 +1,10 @@
-﻿namespace Npgsql.Extension.Reader;
+﻿using System.Data.Common;
+
+namespace Npgsql.Extension.Reader;
 
 public interface IReader<T>
 {
-    /// <summary>
-    /// T - Database Data class, using auto mapping
-    /// Now only UpperCamelCase (data class) to snake_case (database)
-    /// </summary>
-    /// <param name="reader">reader returned when used command.ExecuteReaderAsync</param>
-    /// <returns></returns>
-    public static abstract Task<T?> ReadAsync(NpgsqlDataReader reader);
+    public static abstract Task<T?> ReadAsync(DbDataReader reader);
 
-    public static abstract Task<IEnumerable<T>> ReadListAsync(NpgsqlDataReader reader);
+    public static abstract Task<IEnumerable<T>> ReadListAsync(DbDataReader reader);
 }
